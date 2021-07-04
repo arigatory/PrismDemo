1adea196dc0c85decdb0234b26ac78b11cb811f6
Ivan MSI <arigatory@gmail.com>
2021-07-04 22:33:37 +0300

    Add project files.

---
+++ ModuleA/ModuleAModule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using ModuleA.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleAModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+ 
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            IRegion region = _regionManager.Regions["ContentRegion"];
+
+            var view1 = containerProvider.Resolve<ViewA>();
+            region.Add(view1);
+
+            var view2 = containerProvider.Resolve<ViewA>();
+            view2.Content = new TextBlock()
+            {
+                Text = "Hi from View 2",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            region.Add(view2);
+            region.Activate(view2);
+
+            region.Activate(view1);
+
+            region.Deactivate(view1);
+
+            region.Activate(view2);
+
+            region.Remove(view2);
+
+            region.Activate(view1);
+        }
+    }
+}
---
+++ PrismDemo/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using ModuleA;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using PrismDemo.Core.Regions;
+using PrismDemo.Views;
+
+namespace PrismDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleAModule>();
+        }
+    }
+}
