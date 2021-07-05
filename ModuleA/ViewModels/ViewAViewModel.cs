9c7d382a8d649d11b8dd8e9089cd7496a412cfc4
Ivan MSI <arigatory@gmail.com>
2021-07-06 00:02:30 +0300

    viewmodel locator

---
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ModuleA.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _title = "Hello from ViewAViewModel";
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+    }
+}
